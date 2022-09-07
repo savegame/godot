13e558695dc51f5eeb2f0b047b99b199042f806f
sashikknox <sashikknox@gmail.com>
2022-09-08 00:53:41 +0300

    [sailfishos] basic fixes for sailfishos build

--- drivers/gles2/rasterizer_storage_gles2.cpp
+++ drivers/gles2/rasterizer_storage_gles2.cpp
@@ -100,7 +100,7 @@ GLuint RasterizerStorageGLES2::system_fbo = 0;
 //void *glRenderbufferStorageMultisampleAPPLE;
 //void *glResolveMultisampleFramebufferAPPLE;
 #define glRenderbufferStorageMultisample glRenderbufferStorageMultisampleAPPLE
-#elif defined(ANDROID_ENABLED)
+#elif defined(ANDROID_ENABLED) || defined(SAILFISH_ENABLED)
 
 #include <GLES2/gl2ext.h>
 PFNGLRENDERBUFFERSTORAGEMULTISAMPLEEXTPROC glRenderbufferStorageMultisampleEXT;
@@ -5140,7 +5140,7 @@ void RasterizerStorageGLES2::_render_target_allocate(RenderTarget *rt) {
 		glRenderbufferStorageMultisample(GL_RENDERBUFFER, msaa, color_internal_format, rt->width, rt->height);
 
 		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, rt->multisample_color);
-#elif ANDROID_ENABLED
+#elif defined(ANDROID_ENABLED) || defined(SAILFISH_ENABLED)
 		// Render to a texture in android
 		glGenTextures(1, &rt->multisample_color);
 		glBindTexture(GL_TEXTURE_2D, rt->multisample_color);
@@ -5168,7 +5168,7 @@ void RasterizerStorageGLES2::_render_target_allocate(RenderTarget *rt) {
 
 			glDeleteRenderbuffers(1, &rt->multisample_depth);
 			rt->multisample_depth = 0;
-#ifdef ANDROID_ENABLED
+#if defined(ANDROID_ENABLED) || defined(SAILFISH_ENABLED)
 			glDeleteTextures(1, &rt->multisample_color);
 #else
 			glDeleteRenderbuffers(1, &rt->multisample_color);
@@ -5178,7 +5178,7 @@ void RasterizerStorageGLES2::_render_target_allocate(RenderTarget *rt) {
 
 		glBindRenderbuffer(GL_RENDERBUFFER, 0);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-#ifdef ANDROID_ENABLED
+#if defined(ANDROID_ENABLED) || defined(SAILFISH_ENABLED)
 		glBindTexture(GL_TEXTURE_2D, 0);
 #endif
 
@@ -5415,7 +5415,7 @@ void RasterizerStorageGLES2::_render_target_clear(RenderTarget *rt) {
 
 		glDeleteRenderbuffers(1, &rt->multisample_depth);
 		rt->multisample_depth = 0;
-#ifdef ANDROID_ENABLED
+#if defined(ANDROID_ENABLED) || defined(SAILFISH_ENABLED)
 		glDeleteTextures(1, &rt->multisample_color);
 #else
 		glDeleteRenderbuffers(1, &rt->multisample_color);
